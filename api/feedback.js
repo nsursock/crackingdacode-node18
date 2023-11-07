af253c96208cb92b68794aad41796ad219667b21
Nicolas Sursock <nicolas.sursock@gmail.com>
2023-11-07 04:00:29 +0200

    changed 404 for mail

--- api/feedback.js
+++ api/feedback.js
@@ -6,46 +6,73 @@ const supabase = createClient(
   process.env.SUPABASE_KEY
 )
 
-const table = 'reasons'
-const storageName = process.env.NODE_ENV.startsWith('dev') ? table + '.dev' : table
+
 
 module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
 
-  const { reasons, comment } = req.body;
+  switch (req.query.mode) {
 
-  if (!reasons) {
-    return res.status(400).json({ error: 'Missing data for request' });
-  }
+    case 'view':
+      // Define the view you want to query
+      let viewName = 'feedback'
+      viewName = process.env.NODE_ENV.startsWith('dev') ? viewName + '.dev' : viewName
 
-  let isErrorHappened = false
-
-  try {
-    for (const reason of reasons) {
-      const { data, error } =
-        await supabase.from(storageName).insert({ 
-          reason_label: reason.label, reason_description: reason.description, comment: comment || null })
-        .select()
-
-      if (error) {
-        console.error('Error inserting reason:', error);
-        isErrorHappened = true
+      try {
+        // Query the view to select everything
+        const { data, error } = await supabase.from(viewName).select();
+    
+        if (error) {
+          console.error('Error:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          return res.status(200).json(data);
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
       }
-      // else {
-      //   console.log('Reason inserted:', data[0]);
-      // }
-    }
 
-    if (!isErrorHappened) {
-      return res.status(200).json({ message: 'Feedback submitted successfully' });
-    } else {
-      console.error(error);
-      return res.status(500).json({ error: 'Failed to submit feedback' });
-    }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    default:
+      if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+      }
+
+      const table = 'reasons'
+      const storageName = process.env.NODE_ENV.startsWith('dev') ? table + '.dev' : table
+      const { reasons, comment } = req.body;
+
+      if (!reasons) {
+        return res.status(400).json({ error: 'Missing data for request' });
+      }
+
+      let isErrorHappened = false
+
+      try {
+        for (const reason of reasons) {
+          const { data, error } =
+            await supabase.from(storageName).insert({
+              reason_label: reason.label, reason_description: reason.description, comment: comment || null
+            })
+              .select()
+
+          if (error) {
+            console.error('Error inserting reason:', error);
+            isErrorHappened = true
+          }
+          // else {
+          //   console.log('Reason inserted:', data[0]);
+          // }
+        }
+
+        if (!isErrorHappened) {
+          return res.status(200).json({ message: 'Feedback submitted successfully' });
+        } else {
+          console.error(error);
+          return res.status(500).json({ error: 'Failed to submit feedback' });
+        }
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
   }
 };
