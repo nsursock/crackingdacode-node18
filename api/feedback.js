c8f16daf2ef156f904cce807d75bc18d5cc1b3b1
Nicolas Sursock <nicolas.sursock@gmail.com>
2023-11-07 17:29:07 +0200

    added flowbite apexcharts for feedback, windowing buggy for dates

--- api/feedback.js
+++ api/feedback.js
@@ -6,13 +6,47 @@ const supabase = createClient(
   process.env.SUPABASE_KEY
 )
 
-
+const table = 'reasons'
+const storageName = process.env.NODE_ENV.startsWith('dev') ? table + '.dev' : table
 
 module.exports = async (req, res) => {
 
   switch (req.query.mode) {
 
     case 'view':
+      const { label, range } = JSON.parse(req.body);
+
+      // Calculate the date range based on the selected option
+      const endDate = new Date(); // Current date
+      const startDate = new Date();
+      startDate.setDate(startDate.getDate() + range[0]);
+      endDate.setDate(endDate.getDate() + range[1]);
+
+      // console.log(startDate, endDate);
+
+      try {
+        // You can now use the startDate and endDate in your Supabase query
+        const { data, error } = await supabase
+          .from(storageName)
+          .select()
+          .gt('created_at', startDate.toISOString().split('T').shift())
+          .lt('created_at', endDate.toISOString().split('T').shift())
+
+        if (error) {
+          console.error('Error:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          // console.log(data);
+          return res.status(200).json(data);
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      break
+
+    case 'view2':
       // Define the view you want to query
       let viewName = 'feedback'
       viewName = process.env.NODE_ENV.startsWith('dev') ? viewName + '.dev' : viewName
@@ -20,7 +54,7 @@ module.exports = async (req, res) => {
       try {
         // Query the view to select everything
         const { data, error } = await supabase.from(viewName).select();
-    
+
         if (error) {
           console.error('Error:', error);
           return res.status(500).json({ error: 'Internal Server Error' });
@@ -37,8 +71,6 @@ module.exports = async (req, res) => {
         return res.status(405).json({ error: 'Method Not Allowed' });
       }
 
-      const table = 'reasons'
-      const storageName = process.env.NODE_ENV.startsWith('dev') ? table + '.dev' : table
       const { reasons, comment } = req.body;
 
       if (!reasons) {
